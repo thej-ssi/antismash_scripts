ac0eacd28417b0d5a538ecb3cb8c77861f3af832
thej-ssi <thej@ssi.dk>
2020-06-02 13:59:50 +0200

    smash

---
+++ batch_antismash.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+
+in_dir = sys.argv[1]
+out_dir = sys.argv[2]
+
+in_folders = os.listdir(in_dir)
+
+for folder in in_folders:
+	files = os.listdir(os.path.join(in_dir,folder))
+	prokka_ID = '.'.join(files[1].split('.')[:-1])
+	gbk_path = os.path.join(in_dir,folder,prokka_ID+'.gbk')
+	out_path = os.path.join(out_dir,folder)
+	cmd = "antismash --output-dir " + out_path + " " + gbk_path
+	if os.path.exists(gbk_path):
+		print(cmd)
+	else:
+		print('err ' + cmd)
--- batch_prokka.py
+++ batch_prokka.py
@@ -11,6 +11,6 @@ files = os.listdir(in_dir)
 for file in files:
 	path = os.path.join(in_dir,file)
 	out_path = os.path.join(out_dir,file)
-	cmd = "sbatch -D . -c 4 --mem=12G --time=48:00:00 -J \"spl_prokka\" -p daytime --wrap=\"prokka "+path+" --outdir "+out_path+" --compliant --force\""
+	cmd = "sbatch -D . -c 4 --mem=12G --time=48:00:00 -J \"spl_prokka\" -p project --wrap=\"prokka "+path+" --outdir "+out_path+" --compliant --force\""
 	#print(cmd)
 	os.system(cmd)
