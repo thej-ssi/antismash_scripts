ee4c0fe34b56c7ae591046f855ba049b50f3e610
thej-ssi <thej@ssi.dk>
2020-06-02 14:30:49 +0200

    Update batch_antismash.py

--- batch_antismash.py
+++ batch_antismash.py
@@ -10,12 +10,18 @@ in_folders = os.listdir(in_dir)
 
 for folder in in_folders:
 	if os.path.isdir(os.path.join(in_dir,folder)):
+		missing_gbk_list = []
+		found_gbk_list = []
 		files = os.listdir(os.path.join(in_dir,folder))
 		prokka_ID = '.'.join(files[1].split('.')[:-1])
 		gbk_path = os.path.join(in_dir,folder,prokka_ID+'.gbk')
 		out_path = os.path.join(out_dir,folder)
-		cmd = "antismash --output-dir " + out_path + " " + gbk_path
 		if os.path.exists(gbk_path):
-			print(cmd)
+			cmd = "antismash --output-dir " + out_path + " " + gbk_path
+			sbatch_cmd = "sbatch -D . -c 4 --mem=12G --time=48:00:00 -J \"antismash\" -p project --wrap=\""+cmd+"\""
+			found_gbk_list.append(gbk_path)
+			print(sbatch_cmd)
 		else:
-			print('err ' + cmd)
+			missing_gbk_list.append(gbk_path)
+
+print(missing_gbk_list)
