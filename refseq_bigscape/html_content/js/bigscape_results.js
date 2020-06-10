00949da21d76904fc878eca5606ab77ecf95dcf6
thej-ssi <thej@ssi.dk>
2020-06-10 09:44:55 +0200

    bigscape output

---
+++ refseq_bigscape/html_content/js/bigscape_results.js
@@ -0,0 +1,32 @@
+var bigscape_results = [
+    {
+        "label":"2020-06-10_09-35-54_hybrids_glocal_c0.30",
+        "networks":[
+            {
+                "css":"Others",
+                "label":"Others",
+                "name":"Others"
+            },
+            {
+                "css":"NRPS",
+                "label":"NRPS",
+                "name":"NRPS"
+            },
+            {
+                "css":"RiPPs",
+                "label":"RiPPs",
+                "name":"RiPPs"
+            },
+            {
+                "css":"Terpene",
+                "label":"Terpene",
+                "name":"Terpene"
+            },
+            {
+                "css":"PKSother",
+                "label":"PKSother",
+                "name":"PKSother"
+            }
+        ]
+    }
+];
